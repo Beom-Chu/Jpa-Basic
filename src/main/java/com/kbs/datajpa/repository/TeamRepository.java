a15dd4a7f8819b08c1fecf14f02b98305ed666b3
kbs <chorocsky@gmail.com>
2021-08-31 16:48:23 +0900

    team repository 추가

---
+++ src/main/java/com/kbs/datajpa/repository/TeamRepository.java
@@ -0,0 +1,8 @@
+package com.kbs.datajpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.kbs.datajpa.entity.Team;
+
+public interface TeamRepository extends JpaRepository<Team, Long>{
+
+}
