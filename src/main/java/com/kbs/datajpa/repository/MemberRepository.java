a8189d5213b1c6a4b5ff2f3d2dc52f07da1a4e53
kbs <chorocsky@gmail.com>
2021-09-03 13:18:17 +0900

    값/dto 조회 , 배열조건으로 조회

--- src/main/java/com/kbs/datajpa/repository/MemberRepository.java
+++ src/main/java/com/kbs/datajpa/repository/MemberRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import com.kbs.datajpa.dto.MemberDto;
 import com.kbs.datajpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long>{
+  
   List<Member> findByUserNameAndAgeGreaterThan(String userName, int age);
   
   @Query(name = "Member.findByUserName")
@@ -14,4 +16,13 @@ public interface MemberRepository extends JpaRepository<Member, Long>{
   
   @Query("select m from Member m where m.userName = :userName and m.age = :age")
   List<Member> findUser(@Param("userName") String userName, @Param("age") int age);
+  
+  @Query("select m.userName from Member m")
+  List<String> findUserNameList();
+  
+  @Query("select new com.kbs.datajpa.dto.MemberDto(m.id, m.userName, t.name) from Member m join m.team t")
+  List<MemberDto> findMemberDto();
+  
+  @Query("select m from Member m where m.userName in :names")
+  List<Member> findByNames(@Param("names") List<String> names);
 }
--- src/test/java/com/kbs/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/kbs/datajpa/repository/MemberRepositoryTest.java
@@ -1,13 +1,15 @@
 package com.kbs.datajpa.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import com.kbs.datajpa.dto.MemberDto;
 import com.kbs.datajpa.entity.Member;
+import com.kbs.datajpa.entity.Team;
 
 @SpringBootTest
 @Transactional
@@ -16,6 +18,9 @@ class MemberRepositoryTest {
 
   @Autowired
   MemberRepository memberRepository;
+  
+  @Autowired
+  TeamRepository teamRepository;
   
   @Test
   void testMember() {
@@ -99,5 +104,54 @@ class MemberRepositoryTest {
     List<Member> result = memberRepository.findUser("AAA",10);
     
     assertThat(result.get(0)).isEqualTo(m1);
+  }
+  
+  @Test
+  public void findUserNameList() {
+    Member m1 = new Member("AAA",10);
+    Member m2 = new Member("BBB",20);
+    
+    memberRepository.save(m1);
+    memberRepository.save(m2);
+    
+    List<String> result = memberRepository.findUserNameList();
+    
+    for(String s : result) {
+      System.out.println("s = "+s);
+    }
+  }
+  
+  @Test
+  public void findMemberDto() {
+    
+    Team team = new Team("teamA");
+    teamRepository.save(team);
+    
+    Member m1 = new Member("AAA",10,team);
+    Member m2 = new Member("BBB",20,team);
+    
+    memberRepository.save(m1);
+    memberRepository.save(m2);
+    
+    List<MemberDto> result = memberRepository.findMemberDto();
+    
+    for(MemberDto dto : result) {
+      System.out.println("[[[dto = " + dto);
+    }
+  }
+  
+  @Test
+  public void findByNames() {
+    Member m1 = new Member("AAA",10);
+    Member m2 = new Member("BBB",20);
+    
+    memberRepository.save(m1);
+    memberRepository.save(m2);
+    
+    List<Member> result = memberRepository.findByNames(Arrays.asList("AAA","BBB"));
+    
+    for(Member member : result) {
+      System.out.println("[[[member = "+member);
+    }
   }
 }
